8c7ed883c13864cb3d1c7e3e6c01f3f2615e8d5d
刘鑫沛 <lxp1@meitu.com>
2022-04-18 22:05:34 +0800

    add menu component

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,34 @@
 import React from 'react';
-import Button, { ButtonType, ButtonSize } from './components/Button/button'
-import Alert from './components/Alert/alert';
+import Menu from "./components/Menu/menu";
+import MenuItem from "./components/Menu/menuItem";
+import SubMenu from "./components/Menu/subMenu";
+
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        {/* Button
-        <Button> Hello </Button>
-        <Button disabled> Disabled Button </Button>
-        <Button btnType={ButtonType.Primary} size={ButtonSize.Large}> Large Primary </Button>
-        <Button btnType={ButtonType.Danger} size={ButtonSize.Small}> Small Danger </Button>
-        <Button btnType={ButtonType.Link} href="http://www.baidu.com"> Baidu Link </Button>
-        <Button btnType={ButtonType.Link} href="http://www.baidu.com" disabled> Disabled Link </Button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
-        <Alert closeable title="123" content="123"></Alert>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <Menu defaultIndex='0' onSelect={(index) => {alert(index)}}>
+                    <MenuItem index='0'>
+                        cool link
+                    </MenuItem>
+                    <MenuItem disabled index='1'>
+                        cool link2
+                    </MenuItem>
+                    <SubMenu title="dropdown" index='2'>
+                        <MenuItem index='2-0'>
+                            dropdown 1
+                        </MenuItem>
+                        <MenuItem index='2-1'>
+                            dropdown 2
+                        </MenuItem>
+                    </SubMenu>
+                    <MenuItem index='3'>
+                        cool link3
+                    </MenuItem>
+                </Menu>
+            </header>
+        </div>
+    );
 }
 
 export default App;
